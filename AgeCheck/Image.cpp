da3e226b16d7aac759839a68ac3abec1636985a3
rojin.woeser@gmail.com <rojin.woeser@gmail.com>
2024-04-18 01:31:12 +1000

    General improvement , with the implementation of rule of 5

--- AgeCheck/Image.cpp
+++ AgeCheck/Image.cpp
@@ -7,22 +7,64 @@ Image<T>::Image(std::size_t width, std::size_t height, std::size_t channels):
 
 //destructor
 template<typename T>
-Image<T>::~Image() {}
+Image<T>::~Image() = default;
+
+//copy constructor implementation
+template<typename T>
+Image<T>::Image(const Image<T>& other):
+	data(other.data), width(other.width), height(other.height), channels(other.channels) {}
+
+//copy assignment operator implementation
+template<typename T>
+Image<T>& Image<T>::operator=(const Image<T>& other) {
+	if (this != &other) {
+		data = other.data;
+		width = other.width;
+		height = other.height;
+		channels = other.channels;
+	}
+	return *this;
+}
+
+//move constructor
+template<typename T>
+Image<T>::Image(Image<T>&& other) noexcept :
+	data(std::move(other.data)), width(other.width), height(other.height), channels(other.channels) {
+	other.width = 0;
+	other.height = 0;
+	other.channels = 0;
+}
+
+//move assignment operator
+template<typename T>
+Image<T>& Image<T>::operator=(Image<T>&& other) noexcept {
+	if (this != &other) {
+		data = std::move(other.data);
+		width = other.width;
+		height = other.height;
+		channels = other.channels;
+
+		other.width = 0;
+		other.height = 0;
+		other.channels = 0;
+	}
+	return *this;
+}
 
 //Accessor method
 template<typename T>
 T& Image<T>::at(std::size_t x, std::size_t y, std::size_t channel) {
 	if (x >= width || y >= height || channel >= channels) {
-		throw std::out_of_range("Coordinates out of bounds!")
-	return data[x + y * width + channel * width * height];
+		throw std::out_of_range("Coordinates out of bounds!");
 	}
+	return data[x + y * width + channel * width * height];
 }
 
 //read-only access
 template<typename T>
 const T& Image<T>::at(std::size_t x, std::size_t y, std::size_t channel) const {
 	if (x >= width || y >= height || channel >= channels) {
-		throw std::out_of_range("Coordinates out of bounds!")
-	return data[x + y * width + channel * width * height];
+		throw std::out_of_range("Coordinates out of bounds!");
 	}
+	return data[x + y * width + channel * width * height];
 }
--- AgeCheck/Image.h
+++ AgeCheck/Image.h
@@ -7,12 +7,30 @@
 template <typename T>
 class Image {
 public:
+	//constructor
 	Image(std::size_t width, std::size_t height, std::size_t channels);
+
+	//destructor
 	~Image();
 
+	//copy constructor
+	Image(const Image& other);
+
+	//copy assignment operator 
+	Image& operator=(const Image& other);
+
+	//move constructor
+	Image(Image&& other) noexcept;
+
+	//move assignment operator
+	Image& operator=(Image&& other) noexcept;
+
+
+	//accessors
 	T& at(std::size_t x, std::size_t y, std::size_t channel);
 	const T& at(std::size_t x, std::size_t y, std::size_t channel) const;
 
+	//getters
 	std::size_t getWidth() const { return width; }
 	std::size_t getHeight() const { return height; }
 	std::size_t getChannels() const { return channels; }
